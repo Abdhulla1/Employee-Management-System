cb6395772502a4424960559860032bc97c60e855
Abdhulla <168398639+Abdhulla1@users.noreply.github.com>
2024-10-27 07:15:25 +0530

    Delete Employee-Management-workspace directory

--- Employee-Management-workspace/Employee-Management/src/main/java/com/empmgmt/repository/ProfessionalDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.empmgmt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.empmgmt.entity.ProfessionalDetails;
-
-public interface ProfessionalDetailsRepository extends JpaRepository<ProfessionalDetails, Long> {
-}
